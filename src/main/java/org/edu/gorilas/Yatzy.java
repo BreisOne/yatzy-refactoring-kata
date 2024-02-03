294cd18edb50679317857ea04c72e4ace9b07612
Brais <b.mascat@gmail.com>
2024-02-03 15:44:13 +0100

    feat(Yatzi.twos): functions twos  refactored

--- src/main/java/org/edu/gorilas/Yatzy.java
+++ src/main/java/org/edu/gorilas/Yatzy.java
@@ -25,19 +25,19 @@ public class Yatzy {
         int sum = 0;
         for (int die:dice){
             if(die==1){
-              sum++;
+              sum+=die;
             }
         }
         return sum;
     }
 
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
+    public static int twos(int... dice) {
         int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
+        for (int die:dice){
+            if (die==2){
+                sum+=die;
+            }
+        }
         return sum;
     }
 
